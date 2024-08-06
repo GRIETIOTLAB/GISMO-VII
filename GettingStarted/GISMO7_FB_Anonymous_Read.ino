f25cff022a57a1a97180e0df7c1250a481fd1c83
GRIETIOTLAB <77530550+GRIETIOTLAB@users.noreply.github.com>
2024-08-06 15:38:37 +0530

    Update GISMO7_FB_Anonymous_Read.ino

--- GettingStarted/GISMO7_FB_Anonymous_Read.ino
+++ GettingStarted/GISMO7_FB_Anonymous_Read.ino
@@ -9,11 +9,11 @@
 #include "addons/RTDBHelper.h"
 
 
-#define WIFI_SSID "IDEALab03"
-#define WIFI_PASSWORD "griet@idealab"
+#define WIFI_SSID "xxxxxxxxxxxx"
+#define WIFI_PASSWORD "xxxxxxxxxxxxxxxx"
 
-#define API_KEY "AIzaSyC-zI60FmauZOLGzAw64IRiXKv4swzSLDM"
-#define DATABASE_URL "https://myproject-e6014-default-rtdb.firebaseio.com/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define API_KEY "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+#define DATABASE_URL "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
 FirebaseData fbdo;
 FirebaseAuth auth;
 FirebaseConfig config;
